0c327090bf057173ab21ea61045ecd41a4b3965e
Igor Semenyuk <igor.semenyuk@rolique.io>
2017-02-25 12:35:10 +0200

    reorganize structure

--- run.sh
+++ run.sh
@@ -5,8 +5,5 @@ result=$(echo $result | tr -d "[-_]-")
 result=$(echo $result | tr '[:upper:]' '[:lower:]')
 containerName=$result"_php_1"
 dockerContainerId=$(docker ps -aqf "name=$containerName")
-docker exec -it $dockerContainerId drupal -V
-#docker exec -it $dockerContainerId drupal init -y -n -q --override
-#docker exec -it $dockerContainerId drupal site:new --help
-#docker exec -it $dockerContainerId drupal chain --file=install.yml -y
-#docker exec -it $dockerContainerId ls -la
+docker exec -it $dockerContainerId cp -R /var/www/tmp /var/www/html/web
+docker exec -it $dockerContainerId  chmod 777 -R /var/www/html

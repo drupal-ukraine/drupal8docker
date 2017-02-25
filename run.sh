1a17d3ef7f7000a0c7865e6d7794387bff49f3ba
Igor Semenyuk <igor.semenyuk@rolique.io>
2017-02-25 15:17:52 +0200

    download and install drupal modules

--- run.sh
+++ run.sh
@@ -13,5 +13,11 @@ docker exec -it $dockerContainerId tar -xz --strip-components=1 -f drupal.tar.gz
 docker exec -it $dockerContainerId rm drupal.tar.gz
 docker exec -it $dockerContainerId mkdir -p sites/default/files/tmp sites/default/files/private
 docker exec -it $dockerContainerId cp /var/www/tmp/settings.php /var/www/html/sites/default/
-docker exec -it $dockerContainerId cp /var/www/tmp/settings.php /var/www/html/
 docker exec -it $dockerContainerId chmod 777 -R ./
+docker exec -it $dockerContainerId chown www-data:www-data -R ./
+docker exec -it $dockerContainerId drush dl ctools-8.x-3.0-alpha27 --destination=modules/contrib -y
+docker exec -it $dockerContainerId drush dl layout_plugin-8.x-1.0-alpha23 --destination=modules/contrib -y
+docker exec -it $dockerContainerId drush dl page_manager-8.x-1.0-alpha24 --destination=modules/contrib -y
+docker exec -it $dockerContainerId drush dl panels-8.x-3.0-beta5 --destination=modules/contrib -y
+docker exec -it $dockerContainerId chmod 777 -R ./
+docker exec -it $dockerContainerId drush en ctools layout_plugin page_manager panels page_manager_ui -y

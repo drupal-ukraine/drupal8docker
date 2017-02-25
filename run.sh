c9714eb5119a89aad087ab46e774129154449c0b
Igor Semenyuk <igor.semenyuk@rolique.io>
2017-02-25 14:52:21 +0200

    change run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,11 @@ result=$(echo $result | tr -d "[-_]-")
 result=$(echo $result | tr '[:upper:]' '[:lower:]')
 containerName=$result"_php_1"
 dockerContainerId=$(docker ps -aqf "name=$containerName")
-docker exec -it $dockerContainerId drupal -V
 docker exec -it $dockerContainerId curl -fSL "https://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz" -o drupal.tar.gz
 docker exec -it $dockerContainerId chmod 777 drupal.tar.gz
-docker exec -it $dockerContainerId mkdir web
-docker exec -it $dockerContainerId tar -xz --strip-components=1 -f drupal.tar.gz -C web
+docker exec -it $dockerContainerId tar -xz --strip-components=1 -f drupal.tar.gz
 docker exec -it $dockerContainerId rm drupal.tar.gz
+docker exec -it $dockerContainerId mkdir -p sites/default/files/tmp sites/default/files/private
+docker exec -it $dockerContainerId cp /var/www/tmp/settings.php /var/www/html/sites/default/
+docker exec -it $dockerContainerId cp /var/www/tmp/settings.php /var/www/html/
 docker exec -it $dockerContainerId chmod 777 -R ./

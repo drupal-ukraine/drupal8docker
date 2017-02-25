72299b533218291f66fc14106c155a3a360e1a08
Igor Semenyuk <igor.semenyuk@rolique.io>
2017-02-25 13:53:21 +0200

    download drupal core

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+DRUPAL_VERSION=8.2.6
+
 result=${PWD##*/}
 result=$(echo $result | tr -d "[-_]-")
 result=$(echo $result | tr '[:upper:]' '[:lower:]')
 containerName=$result"_php_1"
 dockerContainerId=$(docker ps -aqf "name=$containerName")
-docker exec -it $dockerContainerId cp -R /var/www/tmp /var/www/html/web
-docker exec -it $dockerContainerId  chmod 777 -R /var/www/html
+docker exec -it $dockerContainerId drupal -V
+docker exec -it $dockerContainerId curl -fSL "https://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz" -o drupal.tar.gz
+docker exec -it $dockerContainerId chmod 777 drupal.tar.gz
+docker exec -it $dockerContainerId mkdir web
+docker exec -it $dockerContainerId tar -xz --strip-components=1 -f drupal.tar.gz -C web
+docker exec -it $dockerContainerId rm drupal.tar.gz
+docker exec -it $dockerContainerId chmod 777 -R ./

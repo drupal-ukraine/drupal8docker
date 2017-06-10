e528c527c88f6af6acf92a9ad6f121a7471efa8e
Dmitry Storozhuk <dmutro.storozhuk@gmail.com>
2017-06-10 16:11:17 +0300

    Move code to /docroot. Try to install docwload and unpack drupal with bash.

--- README_distro.md
+++ README_distro.md
@@ -6,25 +6,45 @@
 
 3. Run in console "docker-compose up -d --build && sh run.sh".
 
+4. chmod +x run.sh
+
 4. Install Drupal:
- - Profile: minimal
+ - Profile: `minimal`
  - DB user, Password, database name: `drupal`
  - Site information: 
-  - name: panels_d8
-  - site email: panels_d8@example.com
+  - name: `panels_d8`
+  - site email: `panels_d8@example.com`
   - SITE MAINTENANCE ACCOUNT: 
-    - user: admin 
-    - pass: admin
-    - email: panels_d8@example.com
+    - user: `admin` 
+    - pass: `admin`
+    - email: `panels_d8@example.com`
   
-Links:
+## Links:
+Site will available on host: http://panels_d8.docker.localhost:8877 or http://localhost:2877
 
-Site will available on host: http://localhost:8999
+Phpmyadmin available on host:  http://pma.panels_d8.docker.localhost:8877 or http://localhost:2878
 
-Phpmyadmin available on host:  http://localhost:8001
+Access to admin side - `admin`:`admin`
 
-Access to admin side - admin:admin
+## Container access:
+`$ docker-compose exec php cd cd web/ drush cr`
+`$ docker-compose exec -it bash`
+
+## Mac Users
+To fix Docker poor performance on macOS use the following workaround based on docker-sync project. The core idea is to replace a standard slow volume with a file synchronizer tool.
+
+1.  Install the tool 
+`$ gem install docker-sync`
+
+2.  Start sync container (may take up to ):
+`$ docker-sync start --daemon`.
+Folowing lines indicates the container is started:
+
+```
+     success  Sync container started
+     success  Starting Docker-Sync in the background
+```
+
+`$ docker-compose -f docker-compose-mac.yml up -d`
 
 More info about docker-compose environment images - https://github.com/wodby/docker4drupal
-
-`$ docker-compose -f docker-compose-mac.yml up --force`
--- run.sh
+++ run.sh
@@ -1,23 +1,37 @@
 #!/bin/sh
 
-DRUPAL_VERSION=8.2.6
+DRUPAL_VERSION=8.3.2
 
 result=${PWD##*/}
 result=$(echo $result | tr -d "[-_]-")
 result=$(echo $result | tr '[:upper:]' '[:lower:]')
 containerName=$result"_php_1"
 dockerContainerId=$(docker ps -aqf "name=$containerName")
-docker exec -it $dockerContainerId curl -fSL "https://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz" -o drupal.tar.gz
-docker exec -it $dockerContainerId chmod 777 drupal.tar.gz
-docker exec -it $dockerContainerId tar -xz --strip-components=1 -f drupal.tar.gz
-docker exec -it $dockerContainerId rm drupal.tar.gz
-docker exec -it $dockerContainerId mkdir -p sites/default/files/tmp sites/default/files/private
-docker exec -it $dockerContainerId cp /var/www/tmp/settings.php /var/www/html/sites/default/
-docker exec -it $dockerContainerId chmod 777 -R ./
-docker exec -it $dockerContainerId chown www-data:www-data -R ./
-docker exec -it $dockerContainerId drush dl ctools-8.x-3.0-alpha27 --destination=modules/contrib -y
-docker exec -it $dockerContainerId drush dl layout_plugin-8.x-1.0-alpha23 --destination=modules/contrib -y
-docker exec -it $dockerContainerId drush dl page_manager-8.x-1.0-alpha24 --destination=modules/contrib -y
-docker exec -it $dockerContainerId drush dl panels-8.x-3.0-beta5 --destination=modules/contrib -y
-docker exec -it $dockerContainerId chmod 777 -R ./
-docker exec -it $dockerContainerId drush en ctools layout_plugin page_manager page_manager_ui panels -y
+
+curl -fSL "https://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz" -o docroot/drupal.tar.gz
+cd docroot/
+chmod 777 drupal.tar.gz
+tar -xz --strip-components=1 -f drupal.tar.gz
+rm drupal.tar.gz
+mkdir -p sites/default/files/tmp sites/default/files/private
+
+#TODO: copy settings.php to sites/default/
+
+
+#docker exec -it $dockerContainerId curl -fSL "https://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz" -o drupal.tar.gz
+
+#docker exec -it $dockerContainerId chmod 777 drupal.tar.gz
+#docker exec -it $dockerContainerId tar -xz --strip-components=1 -f drupal.tar.gz
+#docker exec -it $dockerContainerId rm drupal.tar.gz
+#docker exec -it $dockerContainerId mkdir -p sites/default/files/tmp sites/default/files/private
+#docker exec -it $dockerContainerId cp /var/www/tmp/settings.php /var/www/html/sites/default/
+#docker exec -it $dockerContainerId chmod 777 -R ./
+#docker exec -it $dockerContainerId chown www-data:www-data -R ./
+
+
+#docker exec -it $dockerContainerId drush dl ctools-8.x-3.0-alpha27 --destination=modules/contrib -y
+#docker exec -it $dockerContainerId drush dl layout_plugin-8.x-1.0-alpha23 --destination=modules/contrib -y
+#docker exec -it $dockerContainerId drush dl page_manager-8.x-1.0-alpha24 --destination=modules/contrib -y
+#docker exec -it $dockerContainerId drush dl panels-8.x-3.0-beta5 --destination=modules/contrib -y
+#docker exec -it $dockerContainerId chmod 777 -R ./
+#docker exec -it $dockerContainerId drush en ctools layout_plugin page_manager page_manager_ui panels -y
